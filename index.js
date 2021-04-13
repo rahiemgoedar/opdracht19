4830693f7958b65bb763a9348962f82eca644a9b
rahiemgoedar <79698303+rahiemgoedar@users.noreply.github.com>
2021-04-13 14:13:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/*even oefen met mijn blokcomments schrijven hoor,
+dit is de tweede zin 
+en dit de derde. */
+//dit is een single comment 
+
+/* dit is een grote som zeg..poehh he */
+let keersom = 1230941 * 1823794; //naast de formule, noem je het de inline comment
+console.log(keersom)
+let deelsom = 637263 / 54;
+console.log(deelsom)
